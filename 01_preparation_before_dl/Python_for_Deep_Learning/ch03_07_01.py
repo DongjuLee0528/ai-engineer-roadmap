6e03c51107ab6c2b4038b8cdadffcbe9a67b9331
DongjuLee <dozoo0528@gmail.com>
2026-03-28 19:22:12 +0900

    사전자료형과 집합 자료형

--- 01_preparation_before_dl/Python_for_Deep_Learning/ch03_07_01.py
+++ 01_preparation_before_dl/Python_for_Deep_Learning/ch03_07_01.py
@@ -17,3 +17,13 @@ data['banana'] = '바나나'
 data["carrot"] = "당근"
 for key in data.keys():
         print("key:",key, "value:",data[key])
+#사전 자료형은 특정한 데이터의 등장 횟수를 셀 때 효과적으로 사용할 수 있다.
+data[1,3,3,5,4,3,2,1,4]
+counter = {}
+
+for x in data:
+    if x not in counter:
+        counter[x] = 1
+    else:
+        counter[x] += 1
+print(counter)
