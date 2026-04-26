d55be6d1a2e0f3dd8a6935516f98c298106167dd
DongjuLee <dozoo0528@gmail.com>
2026-04-26 22:27:48 +0900

    참 거짓 자료형

---
+++ 01_preparation_before_dl/Python_for_Deep_Learning/ch03_08_01.py
@@ -0,0 +1,27 @@
+a = True
+b = False
+
+print(a)
+print(b)
+print("---------------------------")
+print(bool(7))
+print(bool(1))
+print(bool(0))
+print(bool("Hello World"))
+print(bool(""))
+print(bool([]))
+print(bool(()))
+
+print("---------------------------")
+print(5<7)
+print(5>7)
+print(5==7)
+print(3!=3)
+print(3<=3)
+
+print("---------------------------")
+print(a or b)
+print(a and b)
+print(not b)
+print( 7 in [3, 5 ,7])
+print( 8 not in [2, 4,6,8])
